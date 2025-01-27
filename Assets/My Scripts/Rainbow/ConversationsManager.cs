ee950f47a6e24d736d217b6904069d4f3bbe8763
AlucardVergil <44729950+AlucardVergil@users.noreply.github.com>
2025-01-27 23:39:44 +0200

    Fixed bugs and improved performance for chats. Also shows pic on bubble contact chats now

--- Assets/My Scripts/Rainbow/ConversationsManager.cs
+++ Assets/My Scripts/Rainbow/ConversationsManager.cs
@@ -437,115 +437,116 @@ public class ConversationsManager : MonoBehaviour
 
     void CreateChatMessage(string messageText, bool isOwnMessage, string contactID)
     {
-        rbContacts.GetAllContactsInRoster(callback =>
+        Contact contact = rbContacts.GetContactFromContactId(contactID);
+
+        // First check if contact can be found in roster for optimization, else look in server. No need to look for contact if it's my own message bcz there is no avatar image shown
+        if (contact != null || isOwnMessage)
         {
-            if (callback.Result.Success)
+            CreateChatEntry(messageText, isOwnMessage, contact);
+        }  
+        else
+        {
+            rbContacts.GetContactFromContactIdFromServer(contactID, callback =>
             {
-                List<Contact> contactList = callback.Data;  // List of contacts
+                CreateChatEntry(messageText, isOwnMessage, callback.Data);
+            });
+        }
+    }
 
-                foreach (Contact contact in contactList)
+    private void CreateChatEntry(string messageText, bool isOwnMessage, Contact contact)
+    {
+        UnityMainThreadDispatcher.Instance().Enqueue(() =>
+        {
+            GameObject newMessage;
+
+            Transform currentlySelectedConversationScrollViewContent = conversationScrollViewContent;
+
+            if (currentSelectedConversation.Type == Conversation.ConversationType.User)
+            {
+                Debug.Log("Peer-to-peer conversation.");
+                currentlySelectedConversationScrollViewContent = conversationScrollViewContent;
+            }
+            else if (currentSelectedConversation.Type == Conversation.ConversationType.Room)
+            {
+                Debug.Log("Room conversation.");
+                currentlySelectedConversationScrollViewContent = GetComponent<BubbleManager>().bubbleConversationScrollViewContent;
+            }
+
+
+            // Instantiate the chat message prefab
+            if (!isOwnMessage)
+            {
+                //newMessage = Instantiate(chatMessagePrefab, conversationScrollViewContent.Find(contactID));
+                //newMessage = Instantiate(chatMessagePrefab, currentlySelectedConversationScrollViewContent);
+
+                #region New Way To Display Contact Entry
+                cancelLoad.Reset();
+                var token = cancelLoad.Token;
+
+                newMessage = Instantiate(chatMessagePrefab, currentlySelectedConversationScrollViewContent);
+
+                if (contact != null)
                 {
-                    if (contact.Id == contactID)
-                    {
-                        UnityMainThreadDispatcher.Instance().Enqueue(() =>
-                        {
-                            GameObject newMessage;
+                    ChatPrefabAvatar entry = newMessage.GetComponent<ChatPrefabAvatar>();
+                    entry.Contact = contact;
 
-                            Transform currentlySelectedConversationScrollViewContent = conversationScrollViewContent;
-
-                            if (currentSelectedConversation.Type == Conversation.ConversationType.User)
-                            {
-                                Debug.Log("Peer-to-peer conversation.");
-                                currentlySelectedConversationScrollViewContent = conversationScrollViewContent;
-                            }
-                            else if (currentSelectedConversation.Type == Conversation.ConversationType.Room)
-                            {
-                                Debug.Log("Room conversation.");
-                                currentlySelectedConversationScrollViewContent = GetComponent<BubbleManager>().bubbleConversationScrollViewContent;
-                            }
-
-
-                            // Instantiate the chat message prefab
-                            if (!isOwnMessage)
-                            {
-                                //newMessage = Instantiate(chatMessagePrefab, conversationScrollViewContent.Find(contactID));
-                                //newMessage = Instantiate(chatMessagePrefab, currentlySelectedConversationScrollViewContent);
-
-                                #region New Way To Display Contact Entry
-                                cancelLoad.Reset();
-                                var token = cancelLoad.Token;
-
-                                newMessage = Instantiate(chatMessagePrefab, currentlySelectedConversationScrollViewContent);
-                                ChatPrefabAvatar entry = newMessage.GetComponent<ChatPrefabAvatar>();
-                                entry.Contact = contact;
-
-                                LoadChatAvatar(entry, contact, token);
-                                #endregion
-
-                                // Get references to the Image and TextMeshProUGUI components in the prefab and Assign the profile photo (avatar) and the message text
-                                //Image profileImage = newMessage.GetComponentInChildren<Image>();
-                                //profileImage.sprite = currentChatMessageAvatar.sprite;
-                            }
-                            else
-                            {
-                                //newMessage = Instantiate(chatMessagePrefabMyself, conversationScrollViewContent.Find(contactID));
-                                newMessage = Instantiate(chatMessagePrefabMyself, currentlySelectedConversationScrollViewContent);
-                            }
-
-                            RectTransform rectTransform = newMessage.GetComponent<RectTransform>();
-
-                            // Change only the width while keeping the height the same
-                            Vector2 newSize = rectTransform.sizeDelta;
-                            newSize.x = 780; // Set the desired width
-                            rectTransform.sizeDelta = newSize;
-
-                            TMP_Text messageTextComponent = newMessage.GetNamedChild("Message").GetComponent<TMP_Text>();
-                            messageTextComponent.text = messageText;
-
-                            // Get the Horizontal Layout Group component from the chat message prefab
-                            HorizontalLayoutGroup layoutGroup = newMessage.GetComponent<HorizontalLayoutGroup>();
-
-                            layoutGroup.childForceExpandWidth = false;
-                            layoutGroup.childControlWidth = false;
-
-                            // Check if the message is sent by the user
-                            if (isOwnMessage)
-                            {
-                                // Align the message to the right (for messages sent by the user)
-                                layoutGroup.childAlignment = TextAnchor.MiddleRight;
-
-                                // Optionally, you can add padding on the right to control the spacing
-                                layoutGroup.padding.right = 0;  // Adjust this value as needed                
-                            }
-                            else
-                            {
-                                // Align the message to the left (for messages received from others)
-                                layoutGroup.childAlignment = TextAnchor.MiddleLeft;
-
-                                // Optionally, add padding on the left
-                                layoutGroup.padding.left = 20;  // Adjust this value as needed
-                            }
-
-
-                            // Force the Canvas to update before scrolling
-                            Canvas.ForceUpdateCanvases();
-
-                            // Set verticalNormalizedPosition to 0 to scroll to the bottom when new message is added
-                            currentlySelectedConversationScrollViewContent.GetComponentInParent<ScrollRect>().verticalNormalizedPosition = 0f;
-
-                        });
-
-                        break;
-                    }
+                    LoadChatAvatar(entry, contact, token);
                 }
+                #endregion
+
+                // Get references to the Image and TextMeshProUGUI components in the prefab and Assign the profile photo (avatar) and the message text
+                //Image profileImage = newMessage.GetComponentInChildren<Image>();
+                //profileImage.sprite = currentChatMessageAvatar.sprite;
             }
             else
             {
-                HandleError(callback.Result);
+                //newMessage = Instantiate(chatMessagePrefabMyself, conversationScrollViewContent.Find(contactID));
+                newMessage = Instantiate(chatMessagePrefabMyself, currentlySelectedConversationScrollViewContent);
             }
+
+            RectTransform rectTransform = newMessage.GetComponent<RectTransform>();
+
+            // Change only the width while keeping the height the same
+            Vector2 newSize = rectTransform.sizeDelta;
+            newSize.x = 780; // Set the desired width
+            rectTransform.sizeDelta = newSize;
+
+            TMP_Text messageTextComponent = newMessage.GetNamedChild("Message").GetComponent<TMP_Text>();
+            messageTextComponent.text = messageText;
+
+            // Get the Horizontal Layout Group component from the chat message prefab
+            HorizontalLayoutGroup layoutGroup = newMessage.GetComponent<HorizontalLayoutGroup>();
+
+            layoutGroup.childForceExpandWidth = false;
+            layoutGroup.childControlWidth = false;
+
+            // Check if the message is sent by the user
+            if (isOwnMessage)
+            {
+                // Align the message to the right (for messages sent by the user)
+                layoutGroup.childAlignment = TextAnchor.MiddleRight;
+
+                // Optionally, you can add padding on the right to control the spacing
+                layoutGroup.padding.right = 0;  // Adjust this value as needed                
+            }
+            else
+            {
+                // Align the message to the left (for messages received from others)
+                layoutGroup.childAlignment = TextAnchor.MiddleLeft;
+
+                // Optionally, add padding on the left
+                layoutGroup.padding.left = 20;  // Adjust this value as needed
+            }
+
+
+            // Force the Canvas to update before scrolling
+            Canvas.ForceUpdateCanvases();
+
+            // Set verticalNormalizedPosition to 0 to scroll to the bottom when new message is added
+            currentlySelectedConversationScrollViewContent.GetComponentInParent<ScrollRect>().verticalNormalizedPosition = 0f;
+
         });
     }
-
 
 
 
@@ -583,6 +584,7 @@ public class ConversationsManager : MonoBehaviour
 
                     if (messagesList[i].Content != null) // check if content is null. These entries are bcz it includes call notifications
                     {
+                        Debug.Log("Test4");
                         if (myContact.Jid_im == messagesList[i].FromJid)
                             CreateChatMessage(messagesList[i].Content, true, conversation.PeerId);
                         else
